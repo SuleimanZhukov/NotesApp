74c2dd357c2ec5d4eca6d287337960c1aee24d99
Suleiman Zhukov <suleimanzhukov@gmail.com>
2021-03-22 17:38:39 +0200

    lesson_7

--- app/src/main/java/com/suleiman/notesapp/MainActivity.java
+++ app/src/main/java/com/suleiman/notesapp/MainActivity.java
@@ -1,12 +1,19 @@
 package com.suleiman.notesapp;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
-import android.content.res.Configuration;
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity implements ListFragment.onFragmentSendDataListener {
 
@@ -15,7 +22,10 @@ public class MainActivity extends AppCompatActivity implements ListFragment.onFr
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.app_bar_main);
+
+        Toolbar toolbar = initToolbar();
+        initDrawer(toolbar);
 
         if (savedInstanceState == null) {
             Fragment listFragment = new ListFragment();
@@ -36,5 +46,55 @@ public class MainActivity extends AppCompatActivity implements ListFragment.onFr
         transaction.add(R.id.fragment_container, fragment);
         transaction.setReorderingAllowed(true);
         transaction.commit();
+    }
+
+    private void initDrawer(Toolbar toolbar) {
+        final DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this,
+                drawer,
+                toolbar,
+                R.string.navigation_drawer_open,
+                R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int id = item.getItemId();
+                if (navigateFragment (id)) {
+                    drawer.closeDrawer(GravityCompat.START);
+                    return true;
+                }
+                return false;
+            }
+        });
+    }
+
+    private Toolbar initToolbar() {
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        return toolbar;
+    }
+
+    private boolean navigateFragment(int id) {
+        switch (id) {
+            case R.id.action_settings: {
+                createAddFragment(new SettingsFragment());
+                return true;
+            }
+            case R.id.action_about: {
+                createAddFragment(new AboutFragment());
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        return super.onOptionsItemSelected(item);
     }
 }
