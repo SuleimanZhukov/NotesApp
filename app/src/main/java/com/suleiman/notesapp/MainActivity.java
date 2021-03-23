121337bdb806e7772f075fb4446dc699bf1a24bd
Suleiman Zhukov <suleimanzhukov@gmail.com>
2021-03-23 16:19:39 +0200

    fixes

--- app/src/main/java/com/suleiman/notesapp/MainActivity.java
+++ app/src/main/java/com/suleiman/notesapp/MainActivity.java
@@ -17,20 +17,21 @@ import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity implements ListFragment.onFragmentSendDataListener {
 
+    private static final String BACK_TAG = "BACK_TAG";
     private Publisher publisher = new Publisher();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.app_bar_main);
-
-        Toolbar toolbar = initToolbar();
-        initDrawer(toolbar);
+        setContentView(R.layout.activity_main);
 
         if (savedInstanceState == null) {
             Fragment listFragment = new ListFragment();
             createAddFragment(listFragment);
         }
+
+        Toolbar toolbar = initToolbar();
+        initDrawer(toolbar);
     }
 
     @Override
@@ -45,26 +46,27 @@ public class MainActivity extends AppCompatActivity implements ListFragment.onFr
         FragmentTransaction transaction = fragmentManager.beginTransaction();
         transaction.add(R.id.fragment_container, fragment);
         transaction.setReorderingAllowed(true);
+        transaction.addToBackStack(BACK_TAG);
         transaction.commit();
     }
 
     private void initDrawer(Toolbar toolbar) {
         final DrawerLayout drawer = findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-                this,
-                drawer,
-                toolbar,
+                this, drawer, toolbar,
                 R.string.navigation_drawer_open,
                 R.string.navigation_drawer_close);
+        android.util.Log.v("TAG", "new ActionBarDrawerToggle");
         drawer.addDrawerListener(toggle);
         toggle.syncState();
 
+        // Обработка навигационного меню
         NavigationView navigationView = findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
                 int id = item.getItemId();
-                if (navigateFragment (id)) {
+                if (navigateFragment(id)){
                     drawer.closeDrawer(GravityCompat.START);
                     return true;
                 }
@@ -82,11 +84,13 @@ public class MainActivity extends AppCompatActivity implements ListFragment.onFr
     private boolean navigateFragment(int id) {
         switch (id) {
             case R.id.action_settings: {
-                createAddFragment(new SettingsFragment());
+                SettingsFragment settingsFragment = new SettingsFragment();
+                createAddFragment(settingsFragment);
                 return true;
             }
             case R.id.action_about: {
-                createAddFragment(new AboutFragment());
+                AboutFragment aboutFragment = new AboutFragment();
+                createAddFragment(aboutFragment);
                 return true;
             }
         }
